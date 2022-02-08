14cb35b8a298cea1bba08877fa13c39ee9b03fba
Dzhavathanov <92082169+sheyhmansur@users.noreply.github.com>
2022-02-08 19:18:13 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -13,24 +13,13 @@ window.addEventListener("DOMContentLoaded", () => {
   "use strict";
 
   let modalState = {};
-  let deadline = "2022-02-17";
+  let deadline = "2022-03-17";
 
   changeModalState(modalState);
   modals();
   tabs(".glazing_slider ", ".glazing_block", ".glazing_content", "active");
-  tabs(
-    ".decoration_slider",
-    ".no_click",
-    ".decoration_content > div > div",
-    "after_click"
-  );
-  tabs(
-    ".balcon_icons",
-    ".balcon_icons_img",
-    ".big_img > img",
-    "do_image_more",
-    "inline-block"
-  );
+  tabs(".decoration_slider", ".no_click", ".decoration_content > div > div", "after_click");
+  tabs(".balcon_icons", ".balcon_icons_img", ".big_img > img", "do_image_more", "inline-block");
   forms(modalState);
   timer(".container1", deadline);
   images();
